d951b6621a2c390f30773203fccb136534718b63
ProgrammerX <mohammad66084@gmail.com>
2026-06-10 12:22:38 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import os
+import subprocess
+import asyncio
+import sqlite3
+import discord
+import aiohttp
+from discord import command.ext
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+  print("Bot Active & Running")
+  continue
+
+@bot.command()
+async def fetch(ctx, url):
+  try:
+    if https:// or http:// in url:
+      
